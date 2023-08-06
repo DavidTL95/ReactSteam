2e78a9264fbd2647fbeead93eef056436386ee57
DavidTL95 <david.torrado95@gmail.com>
2023-08-06 09:23:51 +0200

    Búsqueda por título de juego

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,47 @@
 import React, { useEffect, useState } from "react";
-import { getDeals } from "../../services/apiCalls";
+import { getDeals, getDealsByTitle } from "../../services/apiCalls";
 import { DealCard } from "../../common/DealCard/DealCard";
 import { Col, Container, Row } from "react-bootstrap";
 import './Home.css'
+import { useDebounce } from "use-debounce";
 
 const Home = () => {
   const [deals, setDeals] = useState([]);
+  const [search, setSearch] = useState("");
+  const [debounceSearch] = useDebounce(search, 750);
 
   useEffect(() => {
-    getDeals().then((res) => setDeals(res));
-  }, []);
+    if(debounceSearch){
+      getDealsByTitle(debounceSearch)
+      .then((res) => {
+        setDeals(res)
+      })
+    }else{
+      getDeals().then((res) => setDeals(res));
+    }
+  }, [debounceSearch]);
+
+  const inputHandler = ({target}) => {
+    const {value} = target;
+    setSearch(value);
+  }
 
   return (
     <>
       <Container fluid>
+        <Row className="d-flex justify-content-center m-2">
+          <Col className="d-flex justify-content-center" xs={10} md={6}>
+            <input
+              className="buscador"
+              name="criteria"
+              type="text"
+              placeholder="Search a character"
+              onChange={inputHandler}
+              />
+              {/* {<button onClick={searchHandler}>SEARCH</button>} */}
+          </Col>
+        </Row>
+
         <Row className="contenedorTarjetas justify-content-center">
           {deals?.map((deal) => {
             return (
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -2,8 +2,18 @@ import axios from "axios";
 
 const URL = 'https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15'
 
+const URLByTitle = ('https://www.cheapshark.com/api/1.0/deals?title=')
+
 export const getDeals = async () => {
     let res = await axios.get(URL);
+
+    console.log(res.data);
+
+    return res.data;
+}
+
+export const getDealsByTitle = async (title) => {
+    let res = await axios.get(URLByTitle+title);
 
     console.log(res.data);
 
